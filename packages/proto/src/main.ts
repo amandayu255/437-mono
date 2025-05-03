e316ecff92e61f1432794c8dd0135b9f232fba9a
amandayu255 <amandayu255@gmail.com>
2025-05-02 17:54:30 -0700

    added playlist component

--- packages/proto/src/main.ts
+++ packages/proto/src/main.ts
@@ -1,6 +1,8 @@
 import { define } from "@calpoly/mustang";
 import { MusicaLinkElement } from "./musica-link.js";
+import { MusicaPlaylistElement } from "./musica-playlist.js";
 
 define({
-  "musica-link": MusicaLinkElement
+  "musica-link": MusicaLinkElement,
+  "musica-playlist": MusicaPlaylistElement
 });
