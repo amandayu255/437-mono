06e922605847aa6403df98e068bccc3bd2b797db
amandayu255 <amandayu255@gmail.com>
2025-05-07 20:18:28 -0700

    added components

--- packages/proto/src/main.ts
+++ packages/proto/src/main.ts
@@ -1,8 +1,12 @@
 import { define } from "@calpoly/mustang";
 import { MusicaLinkElement } from "./musica-link.js";
 import { MusicaPlaylistElement } from "./musica-playlist.js";
+import { MusicaCardElement } from "./musica-card.js";
+import { MusicaViewerElement } from "./musica-viewer.js";
 
 define({
   "musica-link": MusicaLinkElement,
-  "musica-playlist": MusicaPlaylistElement
+  "musica-playlist": MusicaPlaylistElement,
+  "musica-card": MusicaCardElement,
+  "musica-viewer": MusicaViewerElement,
 });
