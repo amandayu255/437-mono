9e4f2fb35a14301cbfe22a3654f35ca7900fb2c8
amandayu255 <amandayu255@gmail.com>
2025-05-11 21:12:59 -0700

    update server to use Express for routing and add song endpoints

---
+++ packages/server/src/index.ts
@@ -0,0 +1,39 @@
+import express, { Request, Response } from "express";
+import dotenv from "dotenv";
+import { connect } from "./services/mongo";
+import Songs from "./services/song-svc";
+
+dotenv.config();
+connect("musica");
+
+const app = express();
+const port = process.env.PORT || 3000;
+const staticDir = process.env.STATIC || "public";
+
+app.use(express.static(staticDir));
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
+
+app.get("/songs", async (req: Request, res: Response) => {
+  try {
+    const allSongs = await Songs.index();
+    res.json(allSongs);
+  } catch (error) {
+    res.status(500).send("Error retrieving songs.");
+  }
+});
+
+app.get("/songs/:title", async (req: Request, res: Response) => {
+  try {
+    const song = await Songs.get(req.params.title);
+    if (song) {
+      res.json(song);
+    } else {
+      res.status(404).send("Song not found");
+    }
+  } catch (error) {
+    res.status(500).send("Error retrieving song.");
+  }
+});
