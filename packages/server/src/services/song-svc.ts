ae13fca69def92a11d5d181c2550b5e6e6820c9a
amandayu255 <amandayu255@gmail.com>
2025-05-12 16:45:55 -0700

    added CRUD operations

--- packages/server/src/services/song-svc.ts
+++ packages/server/src/services/song-svc.ts
@@ -1,5 +1,6 @@
 import { Schema, model } from "mongoose";
 import { Song } from "../models/song";
+import { Types } from "mongoose";
 
 const SongSchema = new Schema<Song>(
   {
@@ -7,7 +8,7 @@ const SongSchema = new Schema<Song>(
     artist: { type: String, required: true, trim: true },
     album: { type: String, required: true, trim: true },
     cover: { type: String, required: true },
-    link: { type: String, required: true }
+    link: { type: String, required: true },
   },
   { collection: "songs" }
 );
@@ -18,12 +19,31 @@ function index(): Promise<Song[]> {
   return SongModel.find();
 }
 
-function get(title: String): Promise<Song> {
-    return SongModel.find({ title })
-      .then((list) => list[0])
-      .catch((err) => {
-        throw `${title} Not Found`;
-      });
-  }
+function get(id: string): Promise<Song | null> {
+  return SongModel.findById(new Types.ObjectId(id)).then((result) => {
+    if (!result) throw "Song not found";
+    return result;
+  });
+}
 
-export default { index, get };
+function create(json: Song): Promise<Song> {
+  const t = new SongModel(json);
+  return t.save();
+}
+
+function update(id: string, song: Song): Promise<Song> {
+  return SongModel.findByIdAndUpdate(id, song, {
+    new: true,
+  }).then((updated) => {
+    if (!updated) throw `${id} not updated`;
+    else return updated as Song;
+  });
+}
+
+function remove(id: string): Promise<void> {
+  return SongModel.findByIdAndDelete(id).then((deleted) => {
+    if (!deleted) throw `${id} not deleted`;
+  });
+}
+
+export default { index, get, create, update, remove };
