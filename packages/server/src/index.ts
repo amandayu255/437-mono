1aea88c2e952a262b7c20105b5cfc3906611fb9d
amandayu255 <amandayu255@gmail.com>
2025-05-16 19:02:22 -0700

    add login page with authentication integration and custom routes

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -3,20 +3,29 @@ import dotenv from "dotenv";
 import { connect } from "./services/mongo";
 import Songs from "./services/song-svc";
 import songs from "./routes/songs";
+import auth, { authenticateUser } from "./routes/auth";
+import path from "path";
+
 
 dotenv.config();
 connect("musica");
 
 const app = express();
 const port = process.env.PORT || 3000;
-const staticDir = process.env.STATIC || "public";
+// const staticDir = process.env.STATIC || "public";
+const staticDir = path.resolve(__dirname, "../../proto/dist");
 
 app.use(express.static(staticDir));
 app.use(express.json());
-app.use("/api/songs", songs);
+app.use("/api/songs", authenticateUser, songs);
+app.use("/auth", auth);
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
+});
+
+app.get("/login", (_, res) => {
+  res.sendFile(path.join(staticDir, "login.html"));
 });
 
 app.get("/songs", async (req: Request, res: Response) => {
