2d106bf076ebb1ead4ac4e940ebba03a83d488e5
amandayu255 <amandayu255@gmail.com>
2025-06-04 04:26:22 -0700

    add message type for saving playlists with success and failure callbacks

--- packages/app/src/messages.ts
+++ packages/app/src/messages.ts
@@ -1,7 +1,16 @@
-import { Song } from "server/models";
+import { Song, Playlist } from "server/models";
 
 export type Msg =
   | ["songs/load", {}]
   | ["songs/set", { songs: Song[] }]
   | ["song/select", { songId: string }]
-  | ["playlist/select", { playlistId: string }];
+  | ["playlist/select", { playlistId: string }]
+  | [
+      "playlist/save",
+      {
+        playlistId: string;
+        playlist: Playlist;
+        onSuccess?: () => void;
+        onFailure?: (err: Error) => void;
+      }
+    ];
