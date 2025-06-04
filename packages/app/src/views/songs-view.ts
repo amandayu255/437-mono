0775ac23fbb32dc398484e7a94b5a3484e2e5a0e
amandayu255 <amandayu255@gmail.com>
2025-06-04 04:33:03 -0700

    add logout functionality and style button in songs view

--- packages/app/src/views/songs-view.ts
+++ packages/app/src/views/songs-view.ts
@@ -1,5 +1,5 @@
 import { View } from "@calpoly/mustang";
-import { html } from "lit";
+import { html, css } from "lit";
 import { state } from "lit/decorators.js";
 import { Msg } from "../messages";
 import { Model } from "../model";
@@ -20,8 +20,47 @@ export class SongsViewElement extends View<Model, Msg> {
     this.dispatchMessage(["songs/load", {}]);
   }
 
+  logout() {
+    localStorage.removeItem("token");
+    const event = new CustomEvent("auth:message", {
+      bubbles: true,
+      composed: true,
+      detail: ["auth/signout", { redirect: "/login" }],
+    });
+    this.dispatchEvent(event);
+  }
+
+  static styles = css`
+    .top-right {
+      position: absolute;
+      top: 3rem; /* pushes below the Dark Mode toggle */
+      right: 1rem;
+    }
+
+    button {
+      padding: 0.5rem 1rem;
+      background-color: var(--color-primary, #00449e);
+      color: white;
+      border: none;
+      border-radius: 0.25rem;
+      font-size: 1rem;
+      cursor: pointer;
+    }
+
+    h2 {
+      padding-left: 1rem;
+    }
+
+    ul {
+      padding-left: 2rem;
+    }
+  `;
+
   render() {
     return html`
+      <div class="top-right">
+        <button @click=${this.logout}>Logout</button>
+      </div>
       <h2>Song List</h2>
       <ul>
         ${(this.songs ?? []).map(
@@ -29,5 +68,5 @@ export class SongsViewElement extends View<Model, Msg> {
         )}
       </ul>
     `;
-  }  
+  }
 }
