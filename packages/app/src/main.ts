2e0b3ed5c8a2cf88fe8d18e7b75128d12d7e9294
amandayu255 <amandayu255@gmail.com>
2025-06-04 23:15:36 -0700

    add routing for song, album, and genre edit views

--- packages/app/src/main.ts
+++ packages/app/src/main.ts
@@ -3,79 +3,45 @@ import { html } from "lit";
 import { Msg } from "./messages";
 import { Model, init } from "./model";
 import update from "./update";
+import { HeaderElement } from "./components/blazing-header";
 import "./views/login-view";
 import "./views/register-view";
 import "./views/song-add";
-import { SongsViewElement } from "./views/songs-view";
-import { HeaderElement } from "./components/blazing-header";
-import { PlaylistViewElement } from "./views/playlist-view";
-import { PlaylistEditElement } from "./views/playlist-edit";
-import { AlbumViewElement } from "./views/album-view";
-import { AlbumAddElement } from "./views/album-add";
-import { GenreViewElement } from "./views/genre-view";
-import { GenreAddElement } from "./views/genre-add";
+import "./views/song-edit";
+import "./views/songs-view";
+import "./views/playlist-view";
+import "./views/playlist-edit";
+import "./views/album-view";
+import "./views/album-add";
+import "./views/album-edit";
+import "./views/genre-view";
+import "./views/genre-add";
+import "./views/genre-edit";
+import "./views/home-view";
 
 const routes = [
-  {
-    path: "/app/songs",
-    view: () => html`<songs-view></songs-view>`,
-  },
-  {
-    path: "/app/songs/add",
-    view: () => html`<song-add></song-add>`,
-  },  
-  {
-    path: "/app/playlist/:playlistId",
-    view: (params: any) =>
-      html`<playlist-view playlist-id=${params.playlistId}></playlist-view>`,
-  },
-  {
-    path: "/app/playlist/:playlistId/edit",
-    view: (params: any) =>
-      html`<playlist-edit playlist-id=${params.playlistId}></playlist-edit>`,
-  },
-  {
-    path: "/app/genres",
-    view: () => html`<genre-view></genre-view>`,
-  },
-  {
-    path: "/app/genres/add",
-    view: () => html`<genre-add></genre-add>`,
-  },
-  {
-    path: "/app/genre/:genreId/edit",
-    view: (params: any) =>
-      html`<genre-edit genre-id=${params.genreId}></genre-edit>`,
-  },
-  {
-    path: "/app/albums",
-    view: () => html`<album-view></album-view>`,
-  },
-  {
-    path: "/app/albums/add",
-    view: () => html`<album-add></album-add>`,
-  },
-  {
-    path: "/app/album/:albumId/edit",
-    view: (params: any) =>
-      html`<album-edit album-id=${params.albumId}></album-edit>`,
-  },
-  {
-    path: "/app/playlists",
-    view: () => html`<playlist-view></playlist-view>`,
-  },
-  {
-    path: "/",
-    redirect: "/app/songs",
-  },
-  {
-    path: "/login",
-    view: () => html`<login-view></login-view>`,
-  },
-  {
-    path: "/register",
-    view: () => html`<register-view></register-view>`,
-  },
+  { path: "/app/songs", view: () => html`<songs-view></songs-view>` },
+  { path: "/app/songs/add", view: () => html`<song-add></song-add>` },
+  { path: "/app/songs/:songId/edit", view: (params: any) => html`<song-edit song-id=${params.songId}></song-edit>` },
+
+  { path: "/app/genres", view: () => html`<genre-view></genre-view>` },
+  { path: "/app/genres/add", view: () => html`<genre-add></genre-add>` },
+  { path: "/app/genre/:genreId/edit", view: (params: any) => html`<genre-edit genre-id=${params.genreId}></genre-edit>` },
+
+  { path: "/app/albums", view: () => html`<album-view></album-view>` },
+  { path: "/app/albums/add", view: () => html`<album-add></album-add>` },
+  { path: "/app/album/:albumId/edit", view: (params: any) => html`<album-edit album-id=${params.albumId}></album-edit>` },
+
+  { path: "/app/playlists", view: () => html`<playlist-view></playlist-view>` },
+  { path: "/app/playlist/:playlistId", view: (params: any) => html`<playlist-view playlist-id=${params.playlistId}></playlist-view>` },
+  { path: "/app/playlist/:playlistId/edit", view: (params: any) => html`<playlist-edit playlist-id=${params.playlistId}></playlist-edit>` },
+
+  { path: "/login", view: () => html`<login-view></login-view>` },
+  { path: "/register", view: () => html`<register-view></register-view>` },
+
+  { path: "/", redirect: "/app/songs" },
+
+  { path: "*", redirect: "/app/songs" }
 ];
 
 define({
@@ -91,12 +57,6 @@ define({
       super(update, init, "musica:auth");
     }
   },
-  "songs-view": SongsViewElement,
-  "playlist-view": PlaylistViewElement,
-  "playlist-edit": PlaylistEditElement,
-  "blazing-header": HeaderElement,
-  "album-view": AlbumViewElement,
-  "album-add": AlbumAddElement,
-  "genre-view": GenreViewElement,
-  "genre-add": GenreAddElement,
+
+  "blazing-header": HeaderElement
 });
